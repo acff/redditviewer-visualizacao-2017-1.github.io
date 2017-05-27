04035cfde0d00af96a63577d39f94648b0898c07
Ana Caroline França <acff@cin.ufpe.br>
2017-05-27 17:44:28 -0300

    Update word cloud module (incomplete)

--- VisualizationsCode/WordCloud/wordCloud.js
+++ VisualizationsCode/WordCloud/wordCloud.js
@@ -1,16 +1,15 @@
 class WordCloud{
-	contructor(width, height, frequency_list, container){
+	constructor(width, height, frequency_list, container){
 		this.height = height;
 		this.width = width;
 		this.frequency_list = frequency_list;
-		this.wordCloudGroup = container.append("g");
-
-		this.color = d3.scale.linear()
+		this.wordCloudGroup = container;
+		debugger;
+		this.colorScale = d3.scale.linear()
 			            .domain([0,1,2,3,4,5,6,10,15,20,100])
 			            .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", 
 			            	"#555", "#444", "#333", "#222"]);
 
-		
 		d3.layout.cloud()
     		.size([this.width, this.height])
             .words(frequency_list)
@@ -21,13 +20,18 @@ class WordCloud{
 	}	
     
     draw(words) {
-        this.wordCloudGroup
+    	var that = this;
+		d3.select("g").append("svg")
+            .attr("width", 850)
+            .attr("height", 350)
+            .attr("class", "wordcloud")
+            .append("g")
             .attr("transform", "translate(320,200)")
             .selectAll("text")
             .data(words)
             .enter().append("text")
             .style("font-size", function(d) { return d.size + "px"; })
-            .style("fill", function(d, i) { return color(i); })
+            .style("fill", function(d, i) { return "black"; })
             .attr("transform", function(d) {
                 return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
             })
