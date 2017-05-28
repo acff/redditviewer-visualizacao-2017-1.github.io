cd7761a2dfeff7935f00b25e408060a0c6fdda26
Ana Caroline França <acff@cin.ufpe.br>
2017-05-28 16:37:39 -0300

    Add wordCount script (word cloud)

--- PreprocessData/wordCount.py
+++ PreprocessData/wordCount.py
@@ -1,3 +1,20 @@
+import csv
 import nltk
+from nltk.tokenize import word_tokenize
 
-csv_output = open('wordCount_output.csv', 'wb');
+wordCount_input = open('result.csv', 'rb');
+wordCount_output = open('wordCountTest.csv', 'wb');
+
+reader = csv.reader(wordCount_input)
+writer = csv.writer(wordCount_output);
+
+words = "";
+for row in reader:
+	words += row[2];
+
+#print (words);
+freq = nltk.FreqDist(word_tokenize(words));
+
+for word, frequency in freq.most_common(10):
+		writer.writerow([str(word), frequency]);
+	
